516905f3173bc6300bfa51eb0bac6f3222696dd3
Faheem <shfaheem8920@gmail.com>
2025-12-12 03:25:02 +0500

    template integrate with chnages

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,38 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('/feature', function () {
+    return view('feature');
+});
+// Route::get('/index', function () {
+//     return view('index');
+// });
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/service', function () {
+    return view('service');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/price', function () {
+    return view('price');
+});
+Route::get('/quote', function () {
+    return view('quote');
+});
+Route::get('/team', function () {
+    return view('team');
+});
+Route::get('/testimonial', function () {
+    return view('testimonial');
+});
+
+
+
 
 Route::middleware([
     'auth:sanctum',
