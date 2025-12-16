90588c830e33fb74452708b03f3c48b567d19013
Faheem <shfaheem8920@gmail.com>
2025-12-17 03:37:36 +0500

    error

---
+++ app/Http/Middleware/Usermiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+
+class Usermiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+         if (Auth::user()->role == 'user') {
+           return $next($request);
+       }
+       else {
+        return response()->json("You are not allowed to access this page");
+    }
+    }
+}
